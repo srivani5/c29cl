808543aa19b1809fa60f61aa07483d625722c1fc
danthogi <srivani.danthogi@gmail.com>
2021-04-19 12:47:48 +0530

    first commit

--- Slingshot.js
+++ Slingshot.js
@@ -21,6 +21,7 @@ class SlingShot{
     display(){
         image(this.sling1,200,20);
         image(this.sling2,170,20);
+
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
